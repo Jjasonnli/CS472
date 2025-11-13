7a8c845b0726156efe5ab682ca282862a69af684
jasonli <lijason089@gmail.com>
2025-11-12 20:34:52 -0500

    finished clients added some comments

--- 03-http-client/http.c
+++ 03-http-client/http.c
@@ -35,89 +35,148 @@
 // credit. 
 //--------------------------------------------------------------------------------
 
+/*
+* Purpose: finds a substring inside another string disregarding whether its upper or lower case
+* 
+* Loops through main string and compares lower case values
+* When the first character matches, checks to see if the rest of the substring also matches
+* Puts a pointer to the match or NULL if not found
+*
+* Uses it for HTTP headers
+*
+*/
 char *strcasestr(const char *s, const char *find)
 {
 	char c, sc;
 	size_t len;
 
+    // makes sure string isnt empty
 	if ((c = *find++) != 0) {
+        // lowercase first character of the "find" string
 		c = tolower((unsigned char)c);
 		len = strlen(find);
 		do {
 			do {
+                // loops through the string until we match
 				if ((sc = *s++) == 0)
-					return (NULL);
+					return (NULL); // no match
 			} while ((char)tolower((unsigned char)sc) != c);
+            // compares rest of string
 		} while (strncasecmp(s, find, len) != 0);
-		s--;
+		s--; // pointer to the start of the match string
 	}
+
+    // returns pointer to match or null if not found
 	return ((char *)s);
 }
 
+/*
+* Purpose: finds a substring inside another string but only within a limited length
+* 
+* Works the same as the previous function, however, it doesn't lower case the string
+* and it wont search pass "slen" characters in "s"
+* Puts a pointer to the match or NULL if not found
+* 
+* Searches inside a buffer when you know the size
+*/
 char *strnstr(const char *s, const char *find, size_t slen)
 {
 	char c, sc;
 	size_t len;
-
+    
+    // not an empty string
 	if ((c = *find++) != '\0') {
-		len = strlen(find);
+		len = strlen(find);     // length of substring after the first character
 		do {
 			do {
+                // If we reach end of main string or run out of allowed length, the function stops
 				if ((sc = *s++) == '\0' || slen-- < 1)
 					return (NULL);
-			} while (sc != c);
+			} while (sc != c); // search until first matching character is found
+
+            // not enough characters left to match, stop
 			if (len > slen)
 				return (NULL);
-		} while (strncmp(s, find, len) != 0);
-		s--;
-	}
+		} while (strncmp(s, find, len) != 0); // compares the next "len" values for a full match
+		s--; // pointer to matched substring
+	} 
+    // returns pointer to match or null if not found
 	return ((char *)s);
 }
 
-
+/*
+* Purpose: establishes a TCP connection to host and port, returns the connected socket
+*
+* uses gethostbyname() to put hostname into IP address
+* sockaddr_in addr structure with the IP, host, and AF_INET
+* creates TCP socket
+* attempts to connect to server
+* returns the socket if successful
+*
+*
+* Uses it for IPv4 only
+ */
 int socket_connect(const char *host, uint16_t port){
     struct hostent *hp;
     struct sockaddr_in addr;
     int sock;
 
+    // hostname to IP address
     if((hp = gethostbyname(host)) == NULL){
 		herror("gethostbyname");
 		return -2;
 	}
     
-    
+    // copy ip address to structure
 	bcopy(hp->h_addr_list[0], &addr.sin_addr, hp->h_length);
-	addr.sin_port = htons(port);
-	addr.sin_family = AF_INET;
-	sock = socket(PF_INET, SOCK_STREAM, 0); 
+	addr.sin_port = htons(port); 
+	addr.sin_family = AF_INET; // IPv4 address family
+	sock = socket(PF_INET, SOCK_STREAM, 0); // creates TCP socket
 	
 	if(sock == -1){
 		perror("socket");
-		return -1;
+		return -1; // failed to create socket
 	}
 
+    // connects to server
     if(connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1){
 		perror("connect");
-		close(sock);
+		close(sock); // cleans up socket if it failed
         return -1;
 	}
 
+    // returns connected socket
     return sock;
 }
 
-
+/*
+* Purpose: finds the length of the http header 
+* 
+* searches the response buffer for the header "\r\n\r\n",
+* which marks it the end of the header and beginning of the body
+* uses strnstr to search within the first http_buff_len bytes
+* if found, calculates how many bytes makes up the header
+* if not found, header is incomplete
+*
+* separates the http header from its body when processing data from socket
+*/
 int get_http_header_len(char *http_buff, int http_buff_len){
     char *end_ptr;
     int header_len = 0;
+
+    // looks for end of header
     end_ptr = strnstr(http_buff,HTTP_HEADER_END,http_buff_len);
 
+    // terminator is heading, header is incomplete or invalid
     if (end_ptr == NULL) {
         fprintf(stderr, "Could not find the end of the HTTP header\n");
         return -1;
     }
-
+    
+    // calculate header length
     header_len = (end_ptr - http_buff) + strlen(HTTP_HEADER_END);
 
+    // returns header length
     return header_len;
 }
 
